dd8349d6113960bac5869f2e9e56fb3b536d9961
MarkNovik <novikmark@gmail.com>
2024-12-03 13:45:57 +0200

    Day 3 typed

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,11 +1,15 @@
-object Day3: AOC(3) {
-    override fun part1(input: String): Long =
-        Regex("""mul\((\d+),(\d+)\)""")
-            .findAll(input)
-            .sumOf {
-                val (a, b) = it.destructured
-                a.toLong() * b.toLong()
-            }
+object Day3 : AOC(3) {
+    sealed interface Command {
+        object Do : Command
+        object Dont : Command
+        data class Mul(val a: Long, val b: Long) : Command {
+            val product get() = a * b
+        }
+    }
+
+    override fun part1(input: String): Long = parseInput(input)
+        .filterIsInstance<Command.Mul>()
+        .sumOf(Command.Mul::product)
 
     override fun part2(input: String): Long =
         Regex("""(do|don't|mul)\((?:(\d+),(\d+))?\)""")
@@ -21,4 +25,16 @@ object Day3: AOC(3) {
                 }
             }.first
 
+    fun parseInput(input: String): Sequence<Command> =
+        Regex("""(do|don't|mul)\((?:(\d+),(\d+))?\)""")
+            .findAll(input)
+            .map {
+                val (name, a, b) = it.destructured
+                when (name) {
+                    "do" -> Command.Do
+                    "don't" -> Command.Dont
+                    "mul" -> Command.Mul(a.toLong(), b.toLong())
+                    else -> error("Invalid name `$name` at ${it.range}")
+                }
+            }
 }
