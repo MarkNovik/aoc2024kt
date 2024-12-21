f18467a070cd5a3a94524f8bcddf38d6fa7773a8
Mark Novik <novikmark@gmail.com>
2024-12-21 02:39:37 +0200

    Day 15 fixed tests

--- src/test/kotlin/Day15Test.kt
+++ src/test/kotlin/Day15Test.kt
@@ -6,22 +6,11 @@ class Day15Test {
     private val bigInput = readTestInput(15)
     private val smallInput = readTestInput(151)
 
-    @Test fun part1BigTest() = assertEquals(10092, Day15.part1(bigInput))
-    @Test fun part1SmallTest() = assertEquals(2028, Day15.part1(smallInput))
+    @Test
+    fun part1BigTest() = assertEquals(10092, Day15.part1(bigInput))
+    @Test
+    fun part1SmallTest() = assertEquals(2028, Day15.part1(smallInput))
 
-    private val thirdInput = """#######
-#...#.#
-#.....#
-#..OO@#
-#..O..#
-#.....#
-#######
-
-<vv<<^^<<^^"""
-
-    @Test fun part2Test() {
-        println(Day15.part2(thirdInput))
-        //assert(false)
-        assertEquals(9021, Day15.part2(bigInput))
-    }
+    @Test
+    fun part2Test() = assertEquals(9021, Day15.part2(bigInput))
 }
